9f5ce07767be8ba7cc702a9ef6a00906055f0b49
Miika Säynevirta <mayse@iki.fi>
2014-08-27 16:18:07 +0300

    tekoälyn valintaboksi aloitusmenuun

--- Cowcentration/src/main/java/cowcentration/ui/GraphicStartMenu.java
+++ Cowcentration/src/main/java/cowcentration/ui/GraphicStartMenu.java
@@ -1,26 +1,27 @@
-
 package cowcentration.ui;
 
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
+public class GraphicStartMenu implements Runnable {
 
-
-public class GraphicStartMenu implements Runnable{
-
-        private JFrame frame;
-        private List jTextFields;
+    private JFrame frame;
+    private List jTextFields;
+    
 
     @Override
     public void run() {
- frame = new JFrame("Cowcentration");
+        frame = new JFrame("Cowcentration");
         frame.setPreferredSize(new Dimension(200, 200));
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -28,34 +29,46 @@ public class GraphicStartMenu implements Runnable{
         createComponents(frame.getContentPane());
 
         frame.pack();
-        frame.setVisible(true);    }
+        frame.setVisible(true);
+    }
 
     private void createComponents(Container container) {
-        BoxLayout layout = new BoxLayout(container, BoxLayout.Y_AXIS);
-        container.setLayout(layout);
-        //staattinen pelaajamäärä tällä hetkellä
-    JTextField player1 = new JTextField("Player 1");
-    JTextField player2 = new JTextField("Player 2");
-    JTextField player3 = new JTextField("Player 3");
-    JTextField player4 = new JTextField("Player 4");
-    
-    JButton start = new JButton("Start game");
-    jTextFields = new ArrayList();
-    jTextFields.add(player1);
-    jTextFields.add(player2);
-    jTextFields.add(player3);
-    jTextFields.add(player4);
-    
-    
-    start.addActionListener(new GameStarter(jTextFields, frame));
+        BoxLayout stackvertically = new BoxLayout(container, BoxLayout.Y_AXIS);
+        FlowLayout row = new FlowLayout();
 
-    container.add(player1);
-    container.add(player2);
-    container.add(player3);
-    container.add(player4);
-    container.add(start);
+        container.setLayout(stackvertically);
+        //static amount of players currently
+        jTextFields = new ArrayList();
+        JPanel player1 = addPlayer("Player 1");
+        JPanel player2 = addPlayer("Player 2");
+        JPanel player3 = addPlayer("Player 3");
+        JPanel player4 = addPlayer("Player 4");
 
+        JButton start = new JButton("Start game");
+
+        start.addActionListener(new GameStarter(jTextFields, frame));
+
+        container.add(player1);
+        container.add(player2);
+        container.add(player3);
+        container.add(player4);
+        
+        container.add(start);
 
     }
-    
+
+    private JPanel addPlayer(String name) {
+        JTextField player = new JTextField(name);
+        jTextFields.add(player);
+        JCheckBox playerAi = new JCheckBox("computer");
+        JPanel row = new JPanel();
+        FlowLayout flow = new FlowLayout();
+
+        row.setLayout(flow);
+        row.add(player);
+        row.add(playerAi);
+
+        return row;
+    }
+
 }
